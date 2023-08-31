f459e07c2547ee1b11d1ac94f93dff0d11dedc00
Kamrul Hasan <kamrul@asabd.org>
2023-08-31 17:13:01 +0600

    CRUD without Database layer

---
+++ SuperHeroAPIDotNet7/Controllers/SuperHeroController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPIDotNet7.Models;
+
+namespace SuperHeroAPIDotNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> superHeroes = new List<SuperHero>
+        {
+            new SuperHero {
+                    Id = 1,
+                    Name = "Spider Man",
+                    FirstName = "Peter",
+                    LastName = "Paker",
+                    Place = "New York City"
+            },
+            new SuperHero {
+                    Id = 2,
+                    Name = "Iron Man",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "Malibu"
+            }
+        };
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeros()
+        {
+            return Ok(superHeroes);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetAllHero(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null)
+                return NotFound("Sorry hero doesn't exits");
+            return Ok(hero);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            superHeroes.Add(hero);
+            return Ok(superHeroes);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<SuperHero>> UpdateHero(SuperHero request)
+        {
+            var hero = superHeroes.Find(x => x.Id == request.Id);
+            if (hero is null)
+                return NotFound("Sorry hero doesn't exits");
+            
+            hero.Name = request.Name;
+            hero.FirstName = request.FirstName;
+            hero.LastName = request.LastName;
+            hero.Place = request.Place;
+
+            return Ok(superHeroes);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<SuperHero>> DeleteHero(int id)
+        {
+            var hero = superHeroes.Find(x => x.Id == id);
+            if (hero is null)
+                return NotFound("Sorry hero doesn't exits");
+
+            superHeroes.Remove(hero);
+            return Ok(superHeroes);
+        }
+
+    }
+}
